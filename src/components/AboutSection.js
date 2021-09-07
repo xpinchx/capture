49b6c370f1c7a0cb1ecafc91c474baf4c1f4d21f
xpinchx <dougwelch@gmail.com>
2021-09-07 09:03:31 -0500

    Added wave SVG animation on about us page

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -4,6 +4,8 @@ import home1 from "../img/home1.png";
 import { About, Description, Image, Hide } from "../styles";
 // Framer Motion
 import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../animation";
+import Wave from "./Wave";
 
 const AboutSection = () => {
   return (
@@ -11,62 +13,29 @@ const AboutSection = () => {
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. We have professionals
           with amazing skills.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="guy with a camera" />
+        <motion.img variants={photoAnim} src={home1} alt="guy with a camera" />
       </Image>
+      <Wave />
     </About>
   );
 };
-
-// Styled Components
-// const About = styled.div`
-//   min-height: 90vh;
-//   display: flex;
-//   align-items: center;
-//   justify-content: space-between;
-//   padding: 5rem 10rem;
-//   color: white;
-// `;
-
-// const Description = styled.div`
-//   flex: 1;
-//   padding-right: 5rem;
-
-//   h2 {
-//     font-weight: lighter;
-//   }
-// `;
-
-// const Image = styled.div`
-//   flex: 1;
-//   overflow: hidden;
-
-//   img {
-//     width: 100%;
-//     height: 80vh;
-//     object-fit: cover;
-//   }
-// `;
-
-// const Hide = styled.div`
-//   overflow: hidden;
-// `;
 
 export default AboutSection;
